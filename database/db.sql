3a8ca14151c94286f5b9c75798ebd59272e81c08
FelipeZuquettiPereira <felipe_z_pereira@estudante.sesisenai.org.br>
2026-08-17 11:16:36 -0300

    add - Script

--- database/db.sql
+++ database/db.sql
@@ -1,1 +1,15 @@
-CREATE DATABASE S
+CREATE DATABASE sistema_pratos;
+USE sistema_pratos;
+
+CREATE TABLE usuario (
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL
+ )
+
+CREATE TABLE pratos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    descicao VARCHAR(100)NOT NULL,
+    preco FLOAT NOT NULL,
+    categoria VARCHAR(200) NOT NULL
+);
