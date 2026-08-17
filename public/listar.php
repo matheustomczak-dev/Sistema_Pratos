36732e4c926a72c4b7cba2c04cbdbb26c0fb140c
FelipeZuquettiPereira <felipe_z_pereira@estudante.sesisenai.org.br>
2026-08-17 11:55:29 -0300

    add - listagem

---
+++ public/listar.php
@@ -0,0 +1,62 @@
+<?php
+
+include "../infra/conexao.php";
+$pratos = mysqli_query($conexao, "SELECT * FROM pratos");
+$usuario = mysqli_query($conexao, "SELECT * FROM usuario");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem dos Pratos Cadastrados</title>
+</head>
+
+<body>
+    
+    <header>
+        <h1>Listagem dos Pratos</h1>
+    </header>
+
+    <main>
+
+        <div>
+            
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Responsavel</th>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Preço</th>
+                    <th>Categoria</th>
+                </tr>
+                <?php while ($pratos = mysqli_fetch_assoc($pratos)) { ?>
+                    <tr>
+                        <td><?php echo $pratos["id"] ?></td>
+                        <td><?php echo $usuario["nome"] ?></td>
+                        <td><?php echo $pratos["nome"] ?></td>
+                        <td><?php echo $pratos["descrição"] ?></td>
+                        <td><?php echo $pratos["preço"] ?></td>
+                        <td><?php echo $pratos["categoria"] ?></td>
+
+                        <td>
+                            <a href="public/editar.php?id=<?php echo $pratos["id"] ?>">Editar</a>
+                            <a href="public/excluir.php?id=<?php echo $pratos["id"] ?>">Excluir</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+
+        </div>
+
+    </main>
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
