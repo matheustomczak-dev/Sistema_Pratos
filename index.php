c3aafdc27515e0285ef886fddbf89a03dd33021b
matheustomczak-dev <matheus_tomczak@estudante.sesisenai.org.br>
2026-08-19 09:48:25 -0300

    up - adicionei require no index

--- index.php
+++ index.php
@@ -23,22 +23,22 @@ $pratos = mysqli_query($conexao, "SELECT * FROM pratos");
         <h2>Cadastro De Usuario </h2>
         <form action="public/cadastrar.php" method="POST">
             <label for="Nome_User">Nome do Usuario:</label>
-            <input type="text" name="Nome_User">
+            <input type="text" name="Nome_User" require>
             <br>
             <label for="Email">Email</label>
-            <input type="text" name="Email">
+            <input type="text" name="Email" require>
             <br>
             <label for="nome_prato"> Nome do Prato </label>
-            <input type="text" name="nome_prato">
+            <input type="text" name="nome_prato" require>
             <br>
             <label for="descricao"> Descrição do prato </label>
-            <input type="text" name="descricao">
+            <input type="text" name="descricao" require>
             <br>
             <label for="valor_prato"> Valor do Prato  </label>
-            <input type="number" name="valor_prato">
+            <input type="number" name="valor_prato" require>
             <br>
             <label for="categoria">Categoria do Prato</label>
-            <input type="text" name="categoria">
+            <input type="text" name="categoria" require>
             <br>
             
             <button type="submit">Cadastrar</button>  <button type="submit">Listar</button> 
